de0aa4d6f75bb3855da0a3528bd4ba13aaf38741
Tanmay-57 <111128934+Tanmay-57@users.noreply.github.com>
2024-05-31 13:19:32 +0530

    Add files via upload

---
+++ project/components/footer.php
@@ -0,0 +1,38 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:9619938374"><i class="fas fa-phone"></i><span>9511803801</span></a>
+         <a href="ameya.yeole@somaiya.edu"><i class="fas fa-envelope"></i><span>ameya.yeole@somaiya.edu</span></a>
+         <a href="mailto:tanmay.shidhani@somaiya.edu"><i class="fas fa-envelope"></i><span>tanmay.shidhani@somaiya.edu</span></a>
+         
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>mumbai, india - 400104</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="about.php"><span>about</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="listings.php"><span>all listings</span></a>
+         <a href="saved.php"><span>saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+         <a href="#"><span>pinterest</span><i class="fab fa-pinterest"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>ANS</span> | all rights reserved!</div>
+
+</footer>
+
+<!-- footer section ends -->
